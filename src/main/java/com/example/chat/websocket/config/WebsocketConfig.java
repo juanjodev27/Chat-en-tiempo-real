517f88fc92112d0cd821cd3630db791ef04b90b6
juanjodev27 <juanjogarcete94@gmail.com>
2025-10-25 09:03:10 -0300

    feat: Implementacion de la entidad chatMensaje y la conexion con postgres

--- src/main/java/com/example/chat/websocket/config/WebsocketConfig.java
+++ src/main/java/com/example/chat/websocket/config/WebsocketConfig.java
@@ -1,5 +1,7 @@
 package com.example.chat.websocket.config;
 
+import com.example.chat.websocket.controller.ChatController;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
@@ -9,9 +11,20 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 @EnableWebSocket
 public class WebsocketConfig implements WebSocketConfigurer {
 
+    private final ChatController chatController;
+
+    public WebsocketConfig(ChatController chatController){
+        this.chatController = chatController;
+    }
+
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry){
-        registry.addHandler(new com.example.chat.websocket.controller.ChatController(),"/chat")
+        registry.addHandler(chatController,"/chat")
                 .setAllowedOrigins("*");
+    }
+
+    @Bean
+    public ChatController chatController(){
+        return new ChatController();
     }
 }
